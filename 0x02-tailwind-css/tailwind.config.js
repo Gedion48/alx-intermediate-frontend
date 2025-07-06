bf49f0d70f95ac751a56ed6981491372f4252539
Gedion Fetene <gedionfetene77@gmail.com>
2025-07-07 01:58:59 +0300

    Set up Tailwind CSS with configuration

--- 0x02-tailwind-css/tailwind.config.js
+++ 0x02-tailwind-css/tailwind.config.js
@@ -1,9 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+ HEAD
   content: ["./*.html"],
+
+  content: ["./src/**/*.{html,js}"],
+ f3ed69f (Set up Tailwind CSS with configuration)
   theme: {
     extend: {},
   },
   plugins: [],
 }
+ HEAD
 
+
+ f3ed69f (Set up Tailwind CSS with configuration)
